7e238ea31cc25633a51ca82bfd9222f25b2826cb
Pavel Kuzmich <kuzmich@sovanet.cz>
2024-07-27 14:57:34 +0200

    feat: [blog] Using the blog post model

--- src/example.js
+++ src/example.js
@@ -1,3 +1,16 @@
 import { initDatabase } from './db/init.js'
+import { Post } from './db/models/post.js'
 
-initDatabase()
+await initDatabase()
+
+const post = new Post({
+  title: 'Sample Post',
+  author: 'John Doe',
+  content: 'This is a sample blog post.',
+  tags: ['sample', 'blog', 'mongoose', 'mongodb'],
+})
+
+await post.save()
+
+const posts = await Post.find()
+console.log(posts)
