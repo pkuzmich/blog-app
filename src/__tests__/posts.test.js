8387fc4e165f6ccdbeed60d1f99031f871951850
Pavel Kuzmich <kuzmich@sovanet.cz>
2024-07-28 11:46:43 +0200

    test: defining test cases for the single post, update and delete post functions

--- src/__tests__/posts.test.js
+++ src/__tests__/posts.test.js
@@ -5,6 +5,9 @@ import {
   listAllPosts,
   listPostsByAuthor,
   listPostsByTag,
+  getPostById,
+  updatePost,
+  deletePost,
 } from '../services/posts'
 import { Post } from '../db/models/post'
 
@@ -105,5 +108,69 @@ describe('listing posts', () => {
   test('should be able to filter posts by tag', async () => {
     const posts = await listPostsByTag('nodejs')
     expect(posts.length).toBe(1)
+  })
+})
+
+describe('getting a post', () => {
+  test('should return a full post', async () => {
+    const post = await getPostById(createdSamplePosts[0]._id)
+
+    expect(post.toObject()).toEqual(createdSamplePosts[0].toObject())
+  })
+
+  test('should fail of the id does not exists', async () => {
+    const post = await getPostById('000000000000000000000000')
+    expect(post).toEqual(null)
+  })
+})
+
+describe('updating posts', () => {
+  test('should update the specified property', async () => {
+    await updatePost(createdSamplePosts[0]._id, {
+      author: 'Test Author',
+    })
+    const updatedPost = await Post.findById(createdSamplePosts[0]._id)
+    expect(updatedPost.author).toEqual('Test Author')
+  })
+
+  test('should not update other properties', async () => {
+    await updatePost(createdSamplePosts[0]._id, {
+      author: 'Test Author',
+    })
+    const updatedPost = await Post.findById(createdSamplePosts[0]._id)
+    expect(updatedPost.title).toEqual('Learning Redux')
+  })
+
+  test('should update the updatedAt timestamp', async () => {
+    await updatePost(createdSamplePosts[0]._id, {
+      author: 'Test Author',
+    })
+    const updatedPost = await Post.findById(createdSamplePosts[0]._id)
+    expect(updatedPost.updatedAt.getTime()).toBeGreaterThan(
+      createdSamplePosts[0].updatedAt.getTime(),
+    )
+  })
+
+  test('should fail if the id does not exist', async () => {
+    const post = await updatePost('000000000000000000000000', {
+      author: 'Test  Author',
+    })
+
+    expect(post).toEqual(null)
+  })
+})
+
+describe('deleting posts', () => {
+  test('should remove the post from the database', async () => {
+    const result = await deletePost(createdSamplePosts[0]._id)
+    expect(result.deletedCount).toEqual(1)
+
+    const deletedPost = await Post.findById(createdSamplePosts[0]._id)
+    expect(deletedPost).toEqual(null)
+  })
+
+  test('should fail if the id does not exist', async () => {
+    const result = await deletePost('000000000000000000000000')
+    expect(result.deletedCount).toEqual(0)
   })
 })
